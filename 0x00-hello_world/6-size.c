c6250dc2e803e5f6d472b9d5933ee15be1e21926
Moseddy <moseddy1@gmail.com>
2022-06-30 16:34:19 -0700

    edited my code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,4 +11,5 @@ printf("Size of long int is %i byte(s)\n", sizeof(long int));
 printf("Size of long long int is %i byte(s)\n", sizeof(long long int));
 printf("Size of float is %i byte(s)\n", sizeof(float));
 printf("Size of double is %i byte(s)\n", sizeof(double));
+return (0);
 }
