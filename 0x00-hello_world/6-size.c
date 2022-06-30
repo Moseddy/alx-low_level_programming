6b4b6eaca30593e201cd30541644ac7aea5ca113
Moseddy <moseddy1@gmail.com>
2022-06-30 16:41:41 -0700

    re edited the 6-file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-printf("Size of char is  %i byte(s)\n", sizeof(char));
-printf("Size of int is  %i byte(s)\n", sizeof(int));
-printf("Size of long int is %i byte(s)\n", sizeof(long int));
-printf("Size of long long int is %i byte(s)\n", sizeof(long long int));
-printf("Size of float is %i byte(s)\n", sizeof(float));
-printf("Size of double is %i byte(s)\n", sizeof(double));
+printf("Size of char is: %li byte(s)\n", sizeof(char));
+printf("Size of int is: %li byte(s)\n", sizeof(int));
+printf("Size of long int is: %li byte(s)\n", sizeof(long int));
+printf("Size of long long int is: %li byte(s)\n", sizeof(long long int));
+printf("Size of float is: %li byte(s)\n", sizeof(float));
+printf("Size of double is: %li byte(s)\n", sizeof(double));
 return (0);
 }
