690ae70975f96e422fbf585e8b11ab48c9ce213a
Moseddy <moseddy1@gmail.com>
2022-07-02 15:05:50 -0700

    created the 0-positive file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main -> The main entry to the program
+ * Return: Success always 0
+ */
+int main(void)
+{
+int n;
+if (n > 0)
+{
+printf("%d is positive. ", n);
+}
+elseif(n = 0)
+{
+printf("%d is zero. ", n);
+}
+else
+{
+printf("%d is negative. ", n);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This is the readme file for the project 0x01 if else and while.
