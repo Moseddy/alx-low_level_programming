f6b0f0de95a969cf0641c535ada4235d185ab5d9
Moseddy <moseddy1@gmail.com>
2022-07-02 16:42:10 -0700

    added the 2-print alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main -> This program uses the putchar function to print alphabets
+ * Return: Success always 0
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar("\n");
+return (0);
+}
