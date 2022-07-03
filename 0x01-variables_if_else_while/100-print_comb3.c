f8b44c034a03a966e7d66f34dc913cbfbebce447
Moseddy <moseddy1@gmail.com>
2022-07-02 19:54:33 -0700

    added the 100-file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main -> This program prints the combination of two numbers
+ * Return: Success always 0
+ */
+int main(void)
+{
+	int num;
+	int ch;
+
+	num = 48;
+	ch = 49;
+
+	while (num <= 57)
+	{
+		while (ch <= 57)
+		{
+			if (ch > num)
+			{
+				putchar(num);
+				putchar(ch);
+				if (num != 56 || ch != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			ch++;
+		}
+		num++;
+	}
+
+	return (0);
+}
