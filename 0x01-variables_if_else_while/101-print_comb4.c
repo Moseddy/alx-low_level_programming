03b60e76d375c138a6cccb377d3994ffefa72bb5
Moseddy <moseddy1@gmail.com>
2022-07-04 04:31:45 -0700

    added the 101 print comb 4 file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main -> This program prints a combination of 3 numbers without repetition
+ * Return: Success always 0
+ */
+int main(void)
+{
+	int ch, num1, num2;
+
+	ch = 48;
+
+	while (ch <= 57)
+	{
+		num1 = 49;
+		while (num1 <= 57)
+		{
+			num2 = 50;
+			while (num2 <= 57)
+			{
+				if (ch < num1 || num1 < num2)
+				{
+					putchar(ch);
+					putchar(num1);
+					putchar(num2);
+					putchar(',');
+					putchar(' ');
+				}
+				num2++;
+			}
+			num1++;
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
