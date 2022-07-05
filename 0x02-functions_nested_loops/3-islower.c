0a2b6436dfff890689ec7b8306846fb61fd8f61f
Moseddy <moseddy1@gmail.com>
2022-07-05 15:51:37 -0700

    added the 3-islower file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _islower -> This program returns 0 or 1 depending on c
+ * Return: 0 or 1 depending on the value of c
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
