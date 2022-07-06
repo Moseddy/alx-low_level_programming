c7c4e3526c10491c2e6753abd67a1e9a898cb75a
Moseddy <moseddy1@gmail.com>
2022-07-06 14:15:11 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,29 +23,31 @@ void print_times_table(int n)
 				}
 				else if (k < 10 && j != 0)
 				{
-					_putchar(k + 48);
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(k + 48);
 				}
 				else if (k >= 10 && k < 100)
 				{
-					_putchar((k / 10) + 48);
-					_putchar((k % 10) + 48);
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
 				}
 				else if (k >= 100)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar((k / 100) + 48);
 					_putchar(((k / 10) % 10) + 48);
 					_putchar((k % 10) + 48);
 				}
-				_putchar('\n');
 				j++;
 			}
+			_putchar('\n');
 			i++;
 		}
 	}
