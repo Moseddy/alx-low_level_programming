82f903f1d686c8b46680824567da570bb711be62
Moseddy <moseddy1@gmail.com>
2022-07-06 02:33:17 -0700

    added the 7-print-last-digit file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit -> This function prints the last digit of a number entered
+ * @num: This is the argument of the function
+ * Return: the last numver of the number entered
+ */
+int print_last_digit(int num)
+{
+	int remainder;
+
+	remainder = num % 10;
+	return (remainder);
+}
