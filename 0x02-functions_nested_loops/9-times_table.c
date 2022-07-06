023d3fe75908750ab2ec3b53cdf39459e91737d2
Moseddy <moseddy1@gmail.com>
2022-07-06 04:26:40 -0700

    The 9 time table file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table -> This program prints the times table form 0 - 9
+ * Return: the time table
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (a < 10)
+	{
+		b = 0;
+		while (b < 10)
+		{
+			_putchar((a * b) + 48);
+			_putchar(',');
+			_putchar(' ');
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
