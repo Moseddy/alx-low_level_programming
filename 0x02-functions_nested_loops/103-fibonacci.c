a00a04fa1437ef2dcdc2751bc390364218c28647
Moseddy <moseddy1@gmail.com>
2022-07-06 16:08:54 -0700

    added the 103-file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -> This program prints the fibonacci numbers divisible by 2
+ * Return: Success always 0
+ */
+int main(void)
+{
+	unsigned long sum;
+	unsigned long a, b, c;
+	int i;
+
+	a = 0;
+	b = 1;
+	sum = 0;
+
+	for(i = 0; i <= 33; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c <= 4000000 && (c % 2) == 0)
+		sum = sum + c;
+		printf("%lu\n", sum);
+	}
+	return (0);
+}
