df19aae64b9598f3dec5def5d9ceb093ce33df36
Moseddy <moseddy1@gmail.com>
2022-07-06 02:16:35 -0700

    added the 6-absolute file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs -> prints the absolute number of any number entered
+ * @n: The argument passed into the function
+ * Return: Success the absolute value of n
+ */
+int _abs(int n)
+{
+	int a;
+
+	if (a < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
