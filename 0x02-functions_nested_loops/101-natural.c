af31976e33b2c6b69131e84607835eeda2e0585d
Moseddy <moseddy1@gmail.com>
2022-07-06 14:56:18 -0700

    hot fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-	int i, sum, sum5;
+	int i;
+	unsigned long int sum, sum5;
 
 	sum = 0;
 	sum5 = 0;
@@ -21,6 +22,6 @@ int main(void)
 			sum5 += i;
 		}
 	}
-	printf("%d", sum + sum5);
+	printf("%lu", sum + sum5);
 	return (0);
 }
