f10bdc9cdb86fa64ad3f8303c3ef4e107deb3599
Moseddy <moseddy1@gmail.com>
2022-07-06 16:30:47 -0700

    added the 104-file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main -> This program prints the first98 fivonacci numbers
+ * Return: Success always 0
+ */
+int main(void)
+{
+	unsigned long int a, b, c;
+	int i;
+
+	a = 0;
+	b = 1;
+
+	for (i = 0; i <= 98; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c != 3736710778780434371)
+		{
+			printf("%lu, ", c);
+		}
+		else
+		{
+			printf("%lu\n", c);
+		}
+	}
+}
