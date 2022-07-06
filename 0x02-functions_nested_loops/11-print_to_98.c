5e2e753e06f7bdaa2acef839977c25fc2eb10490
Moseddy <moseddy1@gmail.com>
2022-07-06 05:18:03 -0700

    added thee 11-file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 -> This function prints numbers from any value to 48
+ * @n: This is the integer argument for this function
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n >= 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+}
