ed63a6d8b164139245db8d0cf8b20f8fe96a6d63
Moseddy <moseddy1@gmail.com>
2022-07-06 04:50:58 -0700

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,10 +15,25 @@ void times_table(void)
 		b = 0;
 		while (b < 10)
 		{
-			_putchar((a * b) / 10 + 48);
-			_putchar((a * b) % 10 + 48);
-			_putchar(',');
-			_putchar(' ');
+			c = a * b;
+			if (c == 0)
+			{
+				_putchar(c + 48);
+			}
+			if (c < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + 48);
+			}
+			else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48)
+			}
 			b++;
 		}
 		_putchar('\n');
