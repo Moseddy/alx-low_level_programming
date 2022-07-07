76023165760175b232334d0d29742b14c8c8f207
Moseddy <moseddy1@gmail.com>
2022-07-07 05:01:14 -0700

    debuggin task 1
    ""
    "

---
+++ 0x03-debuggning/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -> This program tests if zero is positive or negative
+ * Return: Success always zero
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
