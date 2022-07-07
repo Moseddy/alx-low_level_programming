eb93be1c5986101509dd5dd84533ab91d839f706
Moseddy <moseddy1@gmail.com>
2022-07-07 05:39:55 -0700

    added some changes

--- 0x03-debuggning/0-main.c
+++ 0x03-debuggning/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main -> This program tests if zero is positive or negative
- * Return: Success always zero
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 
 	positive_or_negative(i);
 	return (0);
