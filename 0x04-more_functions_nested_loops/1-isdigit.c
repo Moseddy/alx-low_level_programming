c6054b20327705832ec7b44852b369fad5291622
Moseddy <moseddy1@gmail.com>
2022-07-08 14:18:16 -0700

    added the 1-isdigit file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit -> This function returns 1 only when a digit is 
+ * entered
+ * @c: The argument of the above function
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
