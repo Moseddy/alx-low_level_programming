16cb4cf2798e4741270b63a4663eab1a1f3d90e9
Moseddy <moseddy1@gmail.com>
2022-07-08 15:42:37 -0700

    added the 5-file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers -> This function prints 0-14 10 times
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			k = j;
+			if (j >= 10)
+			{
+				_putchar(1 + 48);
+				k = j % 10;
+			}
+			_putchar( k + 48);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
