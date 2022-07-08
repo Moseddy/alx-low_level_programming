d947b43c5ac7af1564207ecdf3ccc644e32d29e9
Moseddy <moseddy1@gmail.com>
2022-07-08 14:23:54 -0700

    hot fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit -> This function returns 1 only when a digit is 
+ * _isdigit -> This function returns 1 only when a digit is
  * entered
  * @c: The argument of the above function
  * Return: 0 or 1
