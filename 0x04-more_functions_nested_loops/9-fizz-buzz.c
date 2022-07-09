c42a51c06854d2f1adaf1319094bc1a22d7604bd
Moseddy <moseddy1@gmail.com>
2022-07-09 04:40:19 -0700

    added the fizz buzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz-buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -> prints Fizz for numbers divisible by 3
+ * Buzz for numbers divisible by 5
+ * and FizzBuzz for numbers divisible by 5 and 3
+ *Return: Success always zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+}
